e33b99e9e828d7363c2945645c5fef7680ee9ba4
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2021-09-09 17:17:04 +0430

    Update style & Add top position option

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import css from './styles.module.scss';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-export const Popup = ({ msg = '', className, children, icon, darkMode = false }) => {
+export const Popup = ({
+  msg = '',
+  className,
+  children,
+  icon,
+  darkMode = false,
+  top = false
+}) => {
   const [openPopup, setOpenPopup] = useState(false);
 
   return (
     <div className={css.PopupDiv_PopupTis} >
       <div
+        className={css.Children_PopupTis}
         onMouseOver={() => setOpenPopup(true)}
         onMouseLeave={() => setOpenPopup(false)}
       >
@@ -18,6 +26,7 @@ export const Popup = ({ msg = '', className, children, icon, darkMode = false })
         className={classNames(css.Popup_PopupTis, className, {
           [css.Close_PopupTis]: !openPopup,
           [css.Dark_PopupTis]: darkMode,
+          [css.Top_PopupTis]: top,
         })}
       >
         {icon}
@@ -31,5 +40,6 @@ Popup.propTypes = {
   className: PropTypes.string,
   msg: PropTypes.string,
   icon: PropTypes.any,
-  darkMode: PropTypes.bool
+  darkMode: PropTypes.bool,
+  top: PropTypes.bool
 };
